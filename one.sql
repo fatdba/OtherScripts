bd2a41ee0107095134059a250c8cd759764f41fa
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-25 10:04:06 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,1 +1,9 @@
+{
+  "errorMessage": "Syntax error in module 'lambda_function': expected an indented block (lambda_function.py, line 492)",
+  "errorType": "Runtime.UserCodeSyntaxError",
+  "requestId": "44f5ae7e-da5c-48d4-9203-771cba97e898",
+  "stackTrace": [
+    "  File \"/var/task/lambda_function.py\" Line 492\n            header_list = [str(i) for i in result[0]._asdict().keys()]\n"
+  ]
+}
 
