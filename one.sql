5a707d33b64fe75bb5ad7bba732b1132f684f1c1
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-25 10:07:41 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,9 +1,8 @@
-{
   "errorMessage": "list index out of range",
   "errorType": "IndexError",
-  "requestId": "ad6856f0-840b-4787-a194-64e794d14c14",
+  "requestId": "cbd216f7-ede3-47e7-ad4a-8e00ea343a4a",
   "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 895, in lambda_handler\n    generate_csv_and_pdf_reports_for_the_drift_tables(reporting_db_secrets_client, reporting_db_secret_arn, table_names_list, scan_id)\n",
-    "  File \"/var/task/lambda_function.py\", line 495, in generate_csv_and_pdf_reports_for_the_drift_tables\n    Column_sizes = [font.getsize(str(i)) for i in result[0]._asdict().keys()]\n"
+    "  File \"/var/task/lambda_function.py\", line 896, in lambda_handler\n    generate_csv_and_pdf_reports_for_the_drift_tables(reporting_db_secrets_client, reporting_db_secret_arn, table_names_list, scan_id)\n",
+    "  File \"/var/task/lambda_function.py\", line 496, in generate_csv_and_pdf_reports_for_the_drift_tables\n    Column_sizes = [font.getsize(str(i)) for i in result[0]._asdict().keys()]\n"
   ]
 }
