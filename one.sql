5d7a5631211b70a0ac2f6a2e2025891f1b2242fd
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-25 09:34:09 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,1 +1,6 @@
-
+ERROR: [104]: archive-push command encountered error(s):
+       repo1: [ProtocolError] expected value '2.47' for greeting key 'version' but got '2.42'
+       HINT: is the same version of pgBackRest installed on the local and remote host?
+2023-08-23 13:01:47.592 P00   INFO: archive-push command end: aborted with exception [104]
+2023-08-23 13:01:47.594 EDT [2154]: [1-1]db=,user=,app=,client=LOG:  00000: archive command failed with exit co
+de 104
