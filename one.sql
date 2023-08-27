b9a91a893fec4f6d17d2226df28c1680f9bba09c
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-27 12:00:27 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,4 +1,4 @@
 [ERROR] IndexError: list index out of range
 Traceback (most recent call last):
-  File "/var/task/lambda_function.py", line 582, in lambda_handler
-    scan_id = str(result[0]._asdict()["nextval"])
+  File "/var/task/lambda_function.py", line 582, in lambda_handler
+    scan_id = str(result[0]._asdict()["nextval"])
