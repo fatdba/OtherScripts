8468fb710e54d57eb6eeee1e2cc9cf5d0722c2fe
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-27 10:02:28 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,19 +1,21 @@
-try:
-    # Attempt to execute the query
-    result = pgs.run_query_using_secrets(secrets_client, secret_arn, sql)
-    
-    # Process the result if successful
-    
-except Exception as e:
-    # Handle the exception appropriately
-    error_message = str(e)
-    # Log or handle the error message
-
-    # You can also choose to set a default result or take other actions
-    result = None  # Set a default value for result
-
-# Now you can work with the result, whether it's the query result or a default value
-if result:
-    # Process the result as intended
-else:
-    # Handle the case where the query failed or returned no results
+# Function to run a query in RDS
+def run_query(secrets_client, secret_arn, sql):
+    try:
+        # check if connection to RDS works
+        dbconn = pgs.get_connection(pgs.get_secret_dict(secrets_client, secret_arn, "AWSCURRENT"))
+        if dbconn:
+            result = pgs.run_query_using_secrets(secrets_client, secret_arn, sql)
+            status = f"Successfully executed query"
+            logger.info("%s" % (status))
+            status_code = 0
+            return status_code, status, result
+        else:
+            status = f"Unable to login to server with secret"
+            logger.info("%s" % (status))
+            status_code = -1
+            return status_code, status, None
+    except Exception as e:
+        status = f"Query execution failed: {e}"
+        logger.info("%s" % (status))
+        status_code = -1
+        return status_code, status, None
