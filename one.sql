73c5c091ac22a6905d0ffe43314af6e7658f0cde
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-27 12:04:37 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,24 +1,4 @@
-def run_query(secrets_client, secret_arn, sql):
-    try:
-        # check if connection to RDS works
-        dbconn = pgs.get_connection(pgs.get_secret_dict(secrets_client, secret_arn, "AWSCURRENT"))
-        if dbconn:
-            result = pgs.run_query_using_secrets(secrets_client, secret_arn, sql)
-            status = "Successfully executed query"
-            logger.info("%s" % (status))
-            status_code = 0
-        else:
-            status = f"Unable to login to server with secret"
-            logger.info("%s" % (status))
-            status_code = -1
-            result = []  # Set result to an empty list in case of failure
-    except Exception as e:
-        status = f"Query execution failed: {e}"
-        logger.info("%s" % (status))
-        status_code = -1
-        result = []  # Set result to an empty list in case of failure
-
-    if not result:
-        result = []  # Set result to an empty list if it's None or empty
-    
-    return status_code, status, result
+[ERROR] IndexError: list index out of range
+Traceback (most recent call last):
+  File "/var/task/lambda_function.py", line 584, in lambda_handler
+    scan_id = str(result[0]._asdict()["nextval"])
