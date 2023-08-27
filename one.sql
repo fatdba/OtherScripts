afb8fabcf9ebf7114ce7ecd62f885c878c9887cd
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-27 11:01:31 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,4 +1,21 @@
-[ERROR] ValueError: not enough values to unpack (expected 3, got 2)
-Traceback (most recent call last):
-  File "/var/task/lambda_function.py", line 584, in lambda_handler
-    status_code,status,result=run_query(reporting_db_secrets_client,reporting_db_secret_arn,sql)
+def run_query(secrets_client, secret_arn, sql):
+    try:
+        # check if connection to RDS works
+        dbconn = pgs.get_connection(pgs.get_secret_dict(secrets_client, secret_arn, "AWSCURRENT"))
+        if dbconn:
+            result = pgs.run_query_using_secrets(secrets_client, secret_arn, sql)
+            status = f"Successfully executed query"
+            logger.info("%s" % status)
+            status_code = 0
+        else:
+            status = f"Unable to login to server with secret"
+            logger.info("%s" % status)
+            status_code = -1
+            result = None
+    except Exception as e:
+        status = f"Query execution failed: {e}"
+        logger.info("%s" % status)
+        status_code = -1
+        result = None
+
+    return status_code, status, result
