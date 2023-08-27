f2f7d8810502ad0cb37d7776b6e69aad4c82ab37
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-27 12:03:23 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -4,19 +4,21 @@ def run_query(secrets_client, secret_arn, sql):
         dbconn = pgs.get_connection(pgs.get_secret_dict(secrets_client, secret_arn, "AWSCURRENT"))
         if dbconn:
             result = pgs.run_query_using_secrets(secrets_client, secret_arn, sql)
-            print("result", result)
             status = "Successfully executed query"
             logger.info("%s" % (status))
             status_code = 0
         else:
-            status = f"Unable to login to server with secret "
+            status = f"Unable to login to server with secret"
             logger.info("%s" % (status))
             status_code = -1
-            result = None  # Set result to None in case of failure
+            result = []  # Set result to an empty list in case of failure
     except Exception as e:
-        status = f"Query execution failed: {e} "
+        status = f"Query execution failed: {e}"
         logger.info("%s" % (status))
         status_code = -1
-        result = None  # Set result to None in case of failure
+        result = []  # Set result to an empty list in case of failure
 
+    if not result:
+        result = []  # Set result to an empty list if it's None or empty
+    
     return status_code, status, result
