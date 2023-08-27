bcf01babe3cb042ae89eb9c9cb08e936300519f5
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-27 10:22:12 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -4,3 +4,4 @@ Traceback (most recent call last):
     generate_csv_and_pdf_reports_for_the_drift_tables(reporting_db_secrets_client, reporting_db_secret_arn, table_names_list, scan_id)
   File "/var/task/lambda_function.py", line 430, in generate_csv_and_pdf_reports_for_the_drift_tables
     header_list = [str(i) for i in result[0]._asdict().keys()]
+
