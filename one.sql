2034b72cb4d73fee1658cfe935b4582567c8a75a
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-27 11:41:02 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -3,19 +3,20 @@ def run_query(secrets_client, secret_arn, sql):
         # check if connection to RDS works
         dbconn = pgs.get_connection(pgs.get_secret_dict(secrets_client, secret_arn, "AWSCURRENT"))
         if dbconn:
-            result = pgs.run_query_using_secrets(secrets_client, secret_arn, sql)
-            status = f"Successfully executed query"
-            logger.info("%s" % status)
-            status_code = 0
+            pgs.run_query_using_secrets(secrets_client, secret_arn, sql)
         else:
-            status = f"Unable to login to server with secret"
-            logger.info("%s" % status)
+            status = f"Unable to login to server with secret "
+            logger.info("%s" % (status))
             status_code = -1
-            result = None
+            return status_code, status  # Return only two values
+        result = pgs.run_query_using_secrets(secret_client, secret_arn,sql)
+        status = "Successfully executed query"
+        #logger.info("%s" % (status))
+        status_code = 0
     except Exception as e:
-        status = f"Query execution failed: {e}"
-        logger.info("%s" % status)
+        pass
+        status = f"Query execution failed: {e} "
+        logger.info("%s" % (status))
         status_code = -1
-        result = None
 
-    return status_code, status, result
+    return status_code, status, result  # Return only two values
