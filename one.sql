8fccb88a748a3b65695359095a17def63d9ee66e
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-27 11:47:35 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,4 +1,21 @@
-[ERROR] UnboundLocalError: local variable 'result' referenced before assignment
-Traceback (most recent call last):
-  File "/var/task/lambda_function.py", line 584, in lambda_handler
-    print("result: ", result)
+def run_query(secrets_client, secret_arn, sql):
+    try:
+        # check if connection to RDS works
+        dbconn = pgs.get_connection(pgs.get_secret_dict(secrets_client, secret_arn, "AWSCURRENT"))
+        if dbconn:
+            result = pgs.run_query_using_secrets(secrets_client, secret_arn, sql)
+            status = "Successfully executed query"
+            logger.info("%s" % (status))
+            status_code = 0
+        else:
+            status = f"Unable to login to server with secret "
+            logger.info("%s" % (status))
+            status_code = -1
+            result = None  # Set result to None in case of failure
+    except Exception as e:
+        status = f"Query execution failed: {e} "
+        logger.info("%s" % (status))
+        status_code = -1
+        result = None  # Set result to None in case of failure
+
+    return status_code, status, result
