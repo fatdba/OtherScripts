bbc8cf2590a49a828e772bb4b900d83ea8006de1
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-27 10:39:48 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,9 +1,20 @@
-{
-  "errorMessage": "too many values to unpack (expected 2)",
-  "errorType": "ValueError",
-  "requestId": "c9ef3dd8-ff28-4729-8328-b06782b5fd57",
-  "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 601, in lambda_handler\n    create_or_alter_table(summary_table_parameter_list, summary_table_name)\n",
-    "  File \"/var/task/lambda_function.py\", line 79, in create_or_alter_table\n    status_code,status=run_query(secrets_client,reporting_db_secret_arn,sql)\n"
-  ]
-}
+def create_or_alter_table(parameter_list, table_name):
+    """
+    parameter_list: list of table column headers
+    table_name: name of the table you are going to create
+    """
+    secrets_client = boto3.client('secretsmanager')
+    reporting_db_secret_arn = os.getenv("db_secret_arn")
+    sql = f"""
+    create table if not exists {table_name} (Id int GENERATED ALWAYS AS IDENTITY, RecordedTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, ScanId Varchar(10));
+    """
+    for each in parameter_list:
+        sql += f"""
+        alter table if exists {table_name} add column if not exists {each} varchar(500);
+        """
+    status_code, status = run_query(secrets_client, reporting_db_secret_arn, sql)
+    if status_code == -1:
+        # Handle the error here
+        print("Error:", status)
+    else:
+        print("Success:", status)
