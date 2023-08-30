86b1028fabecc192b64824763aa9eaa16dae9e9e
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-30 10:51:50 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,1 +1,8 @@
-    conn = connect_and_authenticate(secret_dict, port, dbname, use_ssl, host)
+{
+  "errorMessage": "Syntax error in module 'lambda_function': inconsistent use of tabs and spaces in indentation (lambda_function.py, line 56)",
+  "errorType": "Runtime.UserCodeSyntaxError",
+  "requestId": "bdb62410-e11a-4a67-9114-5d8f79434d6b",
+  "stackTrace": [
+    "  File \"/var/task/lambda_function.py\" Line 56\n    \tlogger.info(f\"Attempting login with |{secret_dict['password']}|\")  # Use for TESTING ONLY!\n"
+  ]
+}
