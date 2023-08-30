7b45a6883ba33605c196a29e21d4d1a040db7784
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-30 12:32:23 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,7 +1,7 @@
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-#Get assume role credentials
+# Get assume role credentials
 def get_assume_role_session(sts_role):
     sts_client = boto3.client('sts')
     return sts_client.assume_role(
@@ -11,20 +11,20 @@ def get_assume_role_session(sts_role):
 
 def connect_and_authenticate(secret_dict, port, dbname, use_ssl, host):
     if host == None:
-        host=secret_dict['host']
+        host = secret_dict['host']
     if dbname == None:
         dbname = secret_dict['dbname'] if 'dbname' in secret_dict else "postgres"
 
     # Try to obtain a connection to the db
-	logger.info(f"Attempting login with |{secret_dict['password']}|")  # Use for TESTING ONLY!
+    logger.info(f"Attempting login with |{secret_dict['password']}|")  # Use for TESTING ONLY!
     try:
         if use_ssl:
-			logger.info("Attempting login with SSL")
+            logger.info("Attempting login with SSL")
             # Setting sslmode='verify-full' will verify the server's certificate and check the server's host name
             conn = pgdb.connect(host, user=secret_dict['username'], password=secret_dict['password'], database=dbname, port=port,
                                 connect_timeout=5, sslrootcert='/etc/pki/tls/cert.pem', sslmode='verify-full')
         else:
-			logger.info("Attempting login without SSL")
+            logger.info("Attempting login without SSL")
             conn = pgdb.connect(host, user=secret_dict['username'], password=secret_dict['password'], database=dbname, port=port,
                                 connect_timeout=5, sslmode='disable')
         logger.info("Successfully established %s connection as user '%s' with host: '%s'" % ("SSL/TLS" if use_ssl else "non SSL/TLS", secret_dict['username'], secret_dict['host']))
@@ -38,7 +38,11 @@ def connect_and_authenticate(secret_dict, port, dbname, use_ssl, host):
         elif re.search('no pg_hba.conf entry for host ".+", SSL off', e.args[0]):
             logger.error("Unable to establish SSL/TLS handshake, SSL/TLS is enforced on the host: %s" % secret_dict['host'])
         return None
-        
+
 # Function to run a query in RDS 
-def run_query(secrets_client,secret_arn,sql):
+def run_query(secrets_client, secret_arn, sql):
     try:
+        # Your code for running the query goes here
+        pass
+    except Exception as e:
+        logger.error("An error occurred while running the query: %s" % e)
