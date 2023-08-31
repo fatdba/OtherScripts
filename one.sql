a8671b48449135b7d92977e6ebed7f5236027aca
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-31 14:42:33 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,10 +1,34 @@
 SELECT
-    m.member,
-    r.rolname AS role_name,
-    r.rolcanlogin AS can_login
-FROM
-    pg_roles r
-JOIN
-    pg_auth_members m ON r.oid = m.roleid
-WHERE
-    r.rolsuper IS TRUE;
+    superuser_roles.rolname AS superuser_role,
+    superuser_roles.rolcanlogin,
+    'ALL' AS placeholder1,
+    'ALL' AS placeholder2,
+    'ALL' AS placeholder3,
+    'ALL' AS placeholder4,
+    'SUPERUSER' AS placeholder5,
+    'SERVER' AS placeholder6,
+    assigned_users.other_role_id,
+    assigned_users.other_role_name
+FROM (
+    SELECT
+        a.oid AS superuser_role_id,
+        a.rolname,
+        a.rolcanlogin
+    FROM
+        pg_roles a
+    WHERE
+        a.rolsuper IS TRUE
+) AS superuser_roles
+JOIN (
+    SELECT
+        a.oid AS user_role_id,
+        a.rolname AS user_role_name,
+        b.roleid AS other_role_id,
+        c.rolname AS other_role_name
+    FROM
+        pg_roles a
+    INNER JOIN
+        pg_auth_members b ON a.oid = b.member
+    INNER JOIN
+        pg_roles c ON b.roleid = c.oid
+) AS assigned_users ON superuser_roles.superuser_role_id = assigned_users.other_role_id;
