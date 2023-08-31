2636194a4d2325fbac1223f17994b8b909ae84d0
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-31 14:32:54 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,9 +1,10 @@
-select rolname,'ALL','ALL','ALL','ALL','SUPERUSER','SERVER',rolcanlogin from pg_roles where rolsuper is true;
-
-   select a.oid as user_role_id
-, a.rolname as user_role_name
-, b.roleid as other_role_id
-, c.rolname as other_role_name
-from pg_roles a
-inner join pg_auth_members b on a.oid=b.member
-inner join pg_roles c on b.roleid=c.oid;
+SELECT
+    m.member,
+    r.rolname AS role_name,
+    r.rolcanlogin AS can_login
+FROM
+    pg_roles r
+JOIN
+    pg_auth_members m ON r.oid = m.roleid
+WHERE
+    r.rolsuper IS TRUE;
