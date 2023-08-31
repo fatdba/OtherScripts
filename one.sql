593be4b6a92fea5d7e689c849b9291dd100c28dd
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-08-31 09:39:25 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,7 +1,9 @@
 select rolname,'ALL','ALL','ALL','ALL','SUPERUSER','SERVER',rolcanlogin from pg_roles where rolsuper is true;
 
-select rolname,'ALL','ALL','ALL','ALL','CREATE DATABASE','SERVER',rolcanlogin from pg_roles where rolcreatedb is true and rolsuper is false;
-
-select rolname,'ALL','ALL','ALL','ALL','REPLICATION','SERVER',rolcanlogin from pg_roles where rolreplication is true and rolsuper is false;
-
-select rolname,'ALL','ALL','ALL','ALL','CREATE ROLE','SERVER',rolcanlogin from pg_roles where rolcreaterole is true and rolsuper is false;
+   select a.oid as user_role_id
+, a.rolname as user_role_name
+, b.roleid as other_role_id
+, c.rolname as other_role_name
+from pg_roles a
+inner join pg_auth_members b on a.oid=b.member
+inner join pg_roles c on b.roleid=c.oid;
