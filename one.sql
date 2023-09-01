1c398a7f4aaa44bc8028d2f38a8e111003b41be7
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-01 18:25:41 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,9 +1,5 @@
-{
-  "errorMessage": "tuple indices must be integers or slices, not str",
-  "errorType": "TypeError",
-  "requestId": "777f4a3c-0dc1-4720-bb8c-4bbbe7b8f42c",
-  "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 924, in lambda_handler\n    values = [row[column_name] for column_name in audit_role_privileges_userinfo_parameter_list]\n",
-    "  File \"/var/task/lambda_function.py\", line 924, in <listcomp>\n    values = [row[column_name] for column_name in audit_role_privileges_userinfo_parameter_list]\n"
-  ]
-}
+# Convert the row tuple to a dictionary
+row_dict = {column_name: value for column_name, value in zip(audit_role_privileges_userinfo_parameter_list, row)}
+
+# Access elements using string keys
+values = [row_dict[column_name] for column_name in audit_role_privileges_userinfo_parameter_list]
