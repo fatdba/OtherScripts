95ecd846c0aad2eb11049ebe45552095a4c11144
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-01 17:36:22 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,8 +1,37 @@
-{
-  "errorMessage": "local variable 'audit_role_privileges_userinfo_parameter_list' referenced before assignment",
-  "errorType": "UnboundLocalError",
-  "requestId": "abe3e0b6-b86e-46fe-9725-778cedb388b0",
-  "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 893, in lambda_handler\n    if audit_role_privileges_userinfo_parameter_list:\n"
-  ]
-}
+        # print("rds_list: ", rds_list)
+        if audit_roles_list:
+            flag = 1
+            parameter_list = audit_roles_list[0].keys()
+            #Creates or update audit role privileges table
+            create_or_alter_table(parameter_list, table_name="audit_role_privileges")
+            update_table(audit_roles_list, parameter_list, table_name='audit_role_privileges')
+        if public_roles_list:
+            flag = 2
+            parameter_list = public_roles_list[0].keys()
+            #Creates or update public role privileges table
+            create_or_alter_table(parameter_list, table_name="public_role_privileges")
+            update_table(public_roles_list, parameter_list, table_name='public_role_privileges')
+            
+            audit_role_privileges_userinfo_parameter_list = []
+            audit_role_privileges_userinfo_parameter_list:
+            #flag = 3
+            #parameter_list = audit_roles_list[0].keys()
+            #Creates or update audit role privileges userinfo table
+            audit_role_privileges_userinfo_parameter_list = ["user_role_id", "user_role_name", "other_role_id", "other_role_name"]
+            create_or_alter_table(audit_role_privileges_userinfo_parameter_list, table_name="audit_role_privileges_userinfo")
+            #update_table(audit_roles_list, parameter_list, table_name='audit_role_privileges_userinfo')
+
+    #updates summary table
+    update_table(summary_list, summary_table_parameter_list, summary_table_name)
+    #if flag == 3:
+        #table_names_list = ["audit_role_privileges_userinfo",summary_table_name,"instances_info","snapshots_info"]
+    if flag == 2:
+        table_names_list = ["public_role_privileges", "audit_role_privileges",summary_table_name,"instances_info","snapshots_info"]
+    elif flag == 1:
+        table_names_list = ["audit_role_privileges",summary_table_name,"instances_info","snapshots_info"]
+    else:
+        table_names_list = [summary_table_name,"instances_info","snapshots_info"]
+    #generate CSV and pdf files of the tables for each scan
+    print("flag:", flag)
+    print(table_names_list)
+    generate_csv_and_pdf_reports_for_the_drift_tables(reporting_db_secrets_client, reporting_db_secret_arn, table_names_list, scan_id)
