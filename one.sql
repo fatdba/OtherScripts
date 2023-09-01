9e65fbc4bec8da6fa357d2a0eab73ada55eb89ea
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-01 18:39:31 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,9 +1,16 @@
-{
-  "errorMessage": "list index out of range",
-  "errorType": "IndexError",
-  "requestId": "080d7ca0-5da4-47cb-89ea-2e1c55d9caef",
-  "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 954, in lambda_handler\n    generate_csv_and_pdf_reports_for_the_drift_tables(reporting_db_secrets_client, reporting_db_secret_arn, table_names_list, scan_id)\n",
-    "  File \"/var/task/lambda_function.py\", line 521, in generate_csv_and_pdf_reports_for_the_drift_tables\n    header_list = [str(i) for i in result[0]._asdict().keys()]\n"
-  ]
-}
+def generate_csv_and_pdf_reports_for_the_drift_tables(reporting_db_secrets_client, reporting_db_secret_arn, table_names_list, scan_id):
+    # ... other code ...
+
+    for table_name in table_names_list:
+        # Retrieve data from the database
+        result = pgs.run_query_using_secrets(reporting_db_secrets_client, reporting_db_secret_arn, f"SELECT * FROM {table_name}")
+
+        if not result:
+            # Handle the case where the result is empty (no rows returned)
+            print(f"No data found for {table_name}")
+            continue
+
+        # Generate headers for the CSV and PDF reports
+        header_list = [str(i) for i in result[0]._asdict().keys()]
+        
+        # ... rest of the code for generating reports ...
