1755ca25c677c63b485719e77513f57df6e4185d
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-01 16:43:10 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,13 +1,3 @@
-       # print("rds_list: ", rds_list)
-        if audit_roles_list:
-            flag = 1
-            parameter_list = audit_roles_list[0].keys()
-            #Creates or update audit role privileges table
-            create_or_alter_table(parameter_list, table_name="audit_role_privileges")
-            update_table(audit_roles_list, parameter_list, table_name='audit_role_privileges')
-        if public_roles_list:
-            flag = 2
-            parameter_list = public_roles_list[0].keys()
-            #Creates or update public role privileges table
-            create_or_alter_table(parameter_list, table_name="public_role_privileges")
-            update_table(public_roles_list, parameter_list, table_name='public_role_privileges')
+# Create or alter the audit_role_privileges_userinfo table
+audit_role_privileges_userinfo_parameter_list = ["user_role_id", "user_role_name", "other_role_id", "other_role_name"]
+create_or_alter_table(audit_role_privileges_userinfo_parameter_list, table_name="audit_role_privileges_userinfo")
