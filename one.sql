a8917b0db6979ea8b165dbc36b08ac0792964429
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-01 18:36:05 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,5 +1,9 @@
-# Convert the row tuple to a dictionary
-row_dict = {column_name: value for column_name, value in zip(audit_role_privileges_userinfo_parameter_list, row)}
-
-# Access elements using string keys
-values = [row_dict[column_name] for column_name in audit_role_privileges_userinfo_parameter_list]
+{
+  "errorMessage": "list index out of range",
+  "errorType": "IndexError",
+  "requestId": "080d7ca0-5da4-47cb-89ea-2e1c55d9caef",
+  "stackTrace": [
+    "  File \"/var/task/lambda_function.py\", line 954, in lambda_handler\n    generate_csv_and_pdf_reports_for_the_drift_tables(reporting_db_secrets_client, reporting_db_secret_arn, table_names_list, scan_id)\n",
+    "  File \"/var/task/lambda_function.py\", line 521, in generate_csv_and_pdf_reports_for_the_drift_tables\n    header_list = [str(i) for i in result[0]._asdict().keys()]\n"
+  ]
+}
