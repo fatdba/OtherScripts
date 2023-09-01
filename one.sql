bb4ab298a53a9371ca291651850f872afe0c8206
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-01 16:35:19 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,4 +1,4 @@
-[ERROR] UnboundLocalError: local variable 'result_users' referenced before assignment
-Traceback (most recent call last):
-  File "/var/task/lambda_function.py", line 742, in lambda_handler
-    for row in result_users:
+[ERROR]	2023-09-01T20:33:39.813Z	20b72ba4-c1d1-4d64-ab04-9b04f3da16ad	query_using_secrets: Execption encountered executing query ERROR:  relation "audit_role_privileges_userinfo" does not exist
+LINE 2:         select*from audit_role_privileges_userinfo where sca...
+                            ^
+.
