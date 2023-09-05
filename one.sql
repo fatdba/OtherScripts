9a3622f0db3ab0f8b3c8a987d91904e1b26160bc
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-04 22:37:27 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,14 +1,1 @@
-{
-  "errorMessage": "'Row' object has no attribute 'keys'",
-  "errorType": "AttributeError",
-  "requestId": "9bfc0f65-5993-4bfa-8eba-447fb62bd7d2",
-  "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 917, in lambda_handler\n    parameter_list = users_roles_info[0].keys()\n"
-  ]
-}
-
-        if users_roles_info:
-            flag = 3 
-            parameter_list = users_roles_info[0].keys()
-            create_or_alter_table(parameter_list, table_name="users_roles_info")
-            update_table(users_roles_info, parameter_list, table_name='users_roles_info')
+parameter_list = ["user_role_id", "user_role_name", "other_role_id", "other_role_name"]
