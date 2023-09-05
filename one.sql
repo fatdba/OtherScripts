0fa7e73c2f02dddf83fa96c4863f6808fce2c46b
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-04 21:46:06 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,13 +1,3 @@
-def get_users_roles_info(secrets_client, secret_arn):
-    # SQL query to retrieve user roles information
-    sql = """
-    select a.oid as user_role_id
-    , a.rolname as user_role_name
-    , b.roleid as other_role_id
-    , c.rolname as other_role_name
-    from pg_roles a
-    inner join pg_auth_members b on a.oid=b.member
-    inner join pg_roles c on b.roleid=c.oid;
-    """
-    result = pgs.run_query_using_secrets(secrets_client, secret_arn, sql)
-    return result
+    # Execute SQL query to gather users roles information
+    users_roles_info = get_users_roles_info(secrets_client, rds_secrets_list[index]['DBAdminSecretARN'])
+    
