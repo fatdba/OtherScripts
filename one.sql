b76c9c68426272748ad7123df415d3a06ae0de1c
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-04 22:12:57 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,8 +1,33 @@
-{
-  "errorMessage": "local variable 'users_roles_info' referenced before assignment",
-  "errorType": "UnboundLocalError",
-  "requestId": "26bf1912-bb73-4ec4-be1a-690baa873c68",
-  "stackTrace": [
-    "  File \"/var/task/lambda_function.py\", line 910, in lambda_handler\n    if users_roles_info:\n"
-  ]
-}
+        users_roles_info = []
+        
+        # print("rds_list: ", rds_list)
+        if audit_roles_list:
+            flag = 1
+            parameter_list = audit_roles_list[0].keys()
+            #Creates or update audit role privileges table
+            create_or_alter_table(parameter_list, table_name="audit_role_privileges")
+            update_table(audit_roles_list, parameter_list, table_name='audit_role_privileges')
+        if public_roles_list:
+            flag = 2
+            parameter_list = public_roles_list[0].keys()
+            #Creates or update public role privileges table
+            create_or_alter_table(parameter_list, table_name="public_role_privileges")
+            update_table(public_roles_list, parameter_list, table_name='public_role_privileges')
+        if users_roles_info:
+            flag = 3 
+            parameter_list = users_roles_info[0].keys()
+            create_or_alter_table(parameter_list, table_name="users_roles_info")
+            update_table(users_roles_info, parameter_list, table_name='users_roles_info')
+
+    #updates summary table
+    update_table(summary_list, summary_table_parameter_list, summary_table_name)
+    if flag == 2:
+        table_names_list = ["public_role_privileges", "audit_role_privileges",summary_table_name,"instances_info","snapshots_info"]
+    elif flag == 1:
+        table_names_list = ["audit_role_privileges",summary_table_name,"instances_info","snapshots_info"]
+    else:
+        table_names_list = [summary_table_name,"instances_info","snapshots_info"]
+    #generate CSV and pdf files of the tables for each scan
+    print("flag:", flag)
+    print(table_names_list)
+    generate_csv_and_pdf_reports_for_the_drift_tables(reporting_db_secrets_client, reporting_db_secret_arn, table_names_list, scan_id)
