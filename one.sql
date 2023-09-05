62f4528986bf5367056966f61343a713d60f2dfd
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-04 22:34:38 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,8 +1,14 @@
-if flag == 3:
-    table_names_list = ["public_role_privileges", "audit_role_privileges", "users_roles_info", summary_table_name, "instances_info", "snapshots_info"]
-elif flag == 2:
-    table_names_list = ["public_role_privileges", "audit_role_privileges", summary_table_name, "instances_info", "snapshots_info"]
-elif flag == 1:
-    table_names_list = ["audit_role_privileges", summary_table_name, "instances_info", "snapshots_info"]
-else:
-    table_names_list = [summary_table_name, "instances_info", "snapshots_info"]
+{
+  "errorMessage": "'Row' object has no attribute 'keys'",
+  "errorType": "AttributeError",
+  "requestId": "9bfc0f65-5993-4bfa-8eba-447fb62bd7d2",
+  "stackTrace": [
+    "  File \"/var/task/lambda_function.py\", line 917, in lambda_handler\n    parameter_list = users_roles_info[0].keys()\n"
+  ]
+}
+
+        if users_roles_info:
+            flag = 3 
+            parameter_list = users_roles_info[0].keys()
+            create_or_alter_table(parameter_list, table_name="users_roles_info")
+            update_table(users_roles_info, parameter_list, table_name='users_roles_info')
