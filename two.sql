b17ce66e9e9d3a77d12a3bfea1c220c8babd8b62
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-05 07:45:33 -0400

    Create two.sql

---
+++ two.sql
@@ -0,0 +1,7 @@
+select a.oid as user_role_id
+, a.rolname as user_role_name
+, b.roleid as other_role_id
+, c.rolname as other_role_name
+from pg_roles a
+inner join pg_auth_members b on a.oid=b.member
+inner join pg_roles c on b.roleid=c.oid;
