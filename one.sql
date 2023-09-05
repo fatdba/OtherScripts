683369b5e00f2c61c6756aa17c109d1205d1c1d6
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-04 22:39:32 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,1 +1,9 @@
-parameter_list = ["user_role_id", "user_role_name", "other_role_id", "other_role_name"]
+{
+  "errorMessage": "'Row' object has no attribute 'values'",
+  "errorType": "AttributeError",
+  "requestId": "eff130e8-af60-45c0-abde-81686b823fc7",
+  "stackTrace": [
+    "  File \"/var/task/lambda_function.py\", line 920, in lambda_handler\n    update_table(users_roles_info, parameter_list, table_name='users_roles_info')\n",
+    "  File \"/var/task/lambda_function.py\", line 60, in update_table\n    sql1 += f\"\"\"INSERT INTO {table_name} {parameter_string} VALUES {tuple(each.values())};\"\"\"\n"
+  ]
+}
