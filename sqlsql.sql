bbf8db5f3a93b51433ae5ce6830ed66f676264aa
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 11:42:34 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,5 +1,15 @@
-SELECT r.rolname, current_database(),'DATABASE',fdwname,'FDW', ARRAY[
-(CASE WHEN has_foreign_data_wrapper_privilege(r.rolname,fdwname,'USAGE') THEN 'USAGE' ELSE NULL END)] ,r.rolcanlogin
-FROM pg_catalog.pg_foreign_data_wrapper 
-WHERE has_foreign_data_wrapper_privilege(r.rolname,fdwname,'USAGE')
-and fdwowner <>  r.oid;
+SELECT
+    r.rolname AS role_name,
+    current_database() AS database_name,
+    'DATABASE' AS object_type,
+    fdwname AS object_name,
+    'FDW' AS privilege_type,
+    ARRAY[CASE WHEN has_foreign_data_wrapper_privilege(r.rolname, fdwname, 'USAGE') THEN 'USAGE' ELSE NULL END] AS privileges,
+    r.rolcanlogin AS can_login
+FROM
+    pg_catalog.pg_foreign_data_wrapper
+JOIN
+    pg_catalog.pg_roles r ON fdwowner = r.oid
+WHERE
+    has_foreign_data_wrapper_privilege(r.rolname, fdwname, 'USAGE')
+    AND fdwowner <> r.oid;
