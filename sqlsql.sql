fafa7b5e3a0aab6c5ad218a8675b0ea5a245f178
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 11:45:38 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,4 +1,12 @@
-SELECT r.rolname, current_database(),'DATABASE',l.lanname,'LANGUAGE',
-ARRAY[(CASE WHEN has_language_privilege(r.rolname,lanname,'USAGE') THEN 'USAGE' ELSE NULL END)] ,r.rolcanlogin
-FROM pg_catalog.pg_language l where has_language_privilege(r.rolname,lanname,'USAGE') 
-;
+SELECT
+    r.rolname AS role_name,
+    current_database() AS database_name,
+    'DATABASE' AS object_type,
+    l.lanname AS object_name,
+    'LANGUAGE' AS privilege_type,
+    ARRAY[CASE WHEN has_language_privilege(r.rolname, l.lanname, 'USAGE') THEN 'USAGE' ELSE NULL END] AS privileges,
+    r.rolcanlogin AS can_login
+FROM
+    pg_catalog.pg_language l
+JOIN
+    pg_catalog.pg_roles r ON has_language_privilege(r.rolname, l.lanname, 'USAGE');
