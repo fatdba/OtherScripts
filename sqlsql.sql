a1cb6a6578c5ab1343f1c530c47c93bb27b3f7ff
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 08:41:37 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -5,3 +5,22 @@ where n.nspname not in ('information_schema','pg_catalog','sys')  and c.relkind=
 c.relowner =  r.oid
 AND has_schema_privilege(r.rolname,c.relnamespace,'USAGE')
 ;
+
+SELECT
+    r.rolname,
+    current_database() AS catalog_name,
+    'DATABASE' AS database_name,
+    c.oid::regclass AS table_name,
+    'TABLE' AS level,
+    'TABLE OWNER' AS privilege,
+    r.rolcanlogin
+FROM
+    pg_class c
+JOIN
+    pg_roles r ON c.relowner = r.oid
+JOIN
+    pg_namespace n ON c.relnamespace = n.oid
+WHERE
+    n.nspname NOT IN ('information_schema', 'pg_catalog', 'sys')
+    AND c.relkind = 'r'
+    AND has_schema_privilege(r.rolname, n.oid, 'USAGE');
