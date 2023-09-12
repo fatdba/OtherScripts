c695e69d012a9cb72ef58c8073a43c580d9f46f0
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 09:45:15 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,32 +1,5 @@
-SELECT
-    r.rolname,
-    current_database() AS catalog_name,
-    'DATABASE' AS database_name,
-    c.oid::regclass AS table_name,
-    'TABLE' AS level,
-    ARRAY(
-        SELECT privs
-        FROM unnest(ARRAY[
-            (CASE WHEN has_table_privilege(r.rolname, c.oid, 'SELECT') THEN 'SELECT' ELSE NULL END),
-            (CASE WHEN has_table_privilege(r.rolname, c.oid, 'INSERT') THEN 'INSERT' ELSE NULL END),
-            (CASE WHEN has_table_privilege(r.rolname, c.oid, 'UPDATE') THEN 'UPDATE' ELSE NULL END),
-            (CASE WHEN has_table_privilege(r.rolname, c.oid, 'DELETE') THEN 'DELETE' ELSE NULL END),
-            (CASE WHEN has_table_privilege(r.rolname, c.oid, 'TRUNCATE') THEN 'TRUNCATE' ELSE NULL END),
-            (CASE WHEN has_table_privilege(r.rolname, c.oid, 'REFERENCES') THEN 'REFERENCES' ELSE NULL END),
-            (CASE WHEN has_table_privilege(r.rolname, c.oid, 'TRIGGER') THEN 'TRIGGER' ELSE NULL END)
-        ]) AS privs
-        WHERE privs IS NOT NULL
-    ) AS privileges,
-    r.rolcanlogin
-FROM
-    pg_class c
-JOIN
-    pg_roles r ON c.relowner = r.oid
-JOIN
-    pg_namespace n ON c.relnamespace = n.oid
-WHERE
-    n.nspname NOT IN ('information_schema', 'pg_catalog', 'sys')
-    AND c.relkind = 'r'
-    AND has_table_privilege(r.rolname, c.oid, 'SELECT, INSERT, UPDATE, DELETE, TRUNCATE, REFERENCES, TRIGGER')
-    AND has_schema_privilege(r.rolname, c.relnamespace, 'USAGE')
-    AND c.relowner <> r.oid;
+SELECT  r.rolname,current_database(),'DATABASE',c.oid::regclass,'VIEW','VIEW OWNER' ,
+r.rolcanlogin
+FROM pg_class c JOIN pg_namespace n on c.relnamespace=n.oid where n.nspname not in ('information_schema','pg_catalog','sys') 
+and  c.relkind='v' AND has_schema_privilege(r.rolname,c.relnamespace,'USAGE')
+and c.relowner =  r.oid;
