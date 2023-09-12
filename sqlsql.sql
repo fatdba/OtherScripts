856a638f104825e108242f419b7e75ad5eccf8e1
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 11:32:20 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,7 +1,18 @@
-SELECT r.rolname, current_database(),'DATABASE',c.oid::regclass,'SEQUENCE',
-'SEQUENCE OWNER' ,r.rolcanlogin
-FROM pg_class c JOIN pg_namespace n on c.relnamespace=n.oid where n.nspname not in ('information_schema','pg_catalog','sys') 
-and  c.relkind='S' and
-has_table_privilege(r.rolname,c.oid,'SELECT,UPDATE')  
-AND has_schema_privilege(r.rolname,c.relnamespace,'USAGE')
-and c.relowner =  r.oid;
+SELECT
+  r.rolname,
+  current_database() AS current_db,
+  'DATABASE' AS object_type,
+  c.oid::regclass AS object_name,
+  'SEQUENCE' AS object_kind,
+  'SEQUENCE OWNER' AS object_owner,
+  r.rolcanlogin
+FROM
+  pg_class c
+  JOIN pg_namespace n ON c.relnamespace = n.oid
+  JOIN pg_roles r ON c.relowner = r.oid
+WHERE
+  n.nspname NOT IN ('information_schema', 'pg_catalog', 'sys')
+  AND c.relkind = 'S'
+  AND has_table_privilege(r.rolname, c.oid, 'SELECT, UPDATE')
+  AND has_schema_privilege(r.rolname, c.relnamespace, 'USAGE')
+  AND c.relowner = r.oid;
