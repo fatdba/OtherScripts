092be8935d7204da15f580a12a8e62fde87a98d8
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 08:28:51 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -37,3 +37,24 @@ ERROR:  column u.rolname does not exist
 LINE 10:   SELECT s.schema_name, s.catalog_name, u.rolname, s.schema_...
                                                  ^
 HINT:  Perhaps you meant to reference the column "u.usename".
+
+
+
+
+  SELECT r.rolname, catalog_name, schema_name, 'SCHEMA' AS level, 'DATABASE',
+  ARRAY(
+    SELECT privs
+    FROM unnest(ARRAY[
+        (CASE WHEN has_schema_privilege(r.rolname, c.schema_name, 'CREATE') THEN 'CREATE' ELSE NULL END),
+        (CASE WHEN has_schema_privilege(r.rolname, c.schema_name, 'USAGE') THEN 'USAGE' ELSE NULL END)
+    ])) AS privs,
+  r.rolcanlogin
+FROM (
+  SELECT s.schema_name, s.catalog_name, current_user AS rolname, s.schema_owner
+  FROM information_schema.schemata s
+  WHERE has_schema_privilege(current_user, s.schema_name, 'CREATE,USAGE')
+    AND s.schema_name NOT LIKE 'pg_temp%'
+    AND s.schema_owner <> current_user
+) AS c
+JOIN pg_roles r ON r.rolname = c.rolname;
+
