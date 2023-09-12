85e68327fb85953e979749533f6dd8a2991b17ba
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 11:53:04 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,29 +1,64 @@
-with elevated_perm_proc as
-(
-SELECT  row_number() over( order by p.oid),p.oid,nspname,proname,format_type(unnest(proargtypes)::oid,NULL)
-FROM pg_proc p JOIN pg_namespace n ON p.pronamespace = n.oid JOIN
-pg_authid a ON a.oid = p.proowner 
-WHERE prosecdef OR NOT proconfig IS NULL
+WITH elevated_perm_procs AS (
+    SELECT
+        row_number() OVER (ORDER BY p.oid) AS row_num,
+        p.oid,
+        nspname,
+        proname,
+        format_type(unnest(proargtypes)::oid, NULL) AS format_type
+    FROM
+        pg_proc p
+    JOIN
+        pg_namespace n ON p.pronamespace = n.oid
+    JOIN
+        pg_authid a ON a.oid = p.proowner
+    WHERE
+        prosecdef OR NOT proconfig IS NULL
 ),
-
-func_with_elvated_priv as
-(
-select  oid,nspname,proname,array_to_string(array_agg(format_type),',') as proc_param
-from elevated_perm_proc
-group by oid,nspname,proname
-union
-select p.oid,nspname,proname,' ' as proc_param
-FROM pg_proc p JOIN pg_namespace n ON p.pronamespace = n.oid JOIN
-pg_authid a ON a.oid = p.proowner 
-WHERE (prosecdef OR NOT proconfig IS NULL)
-and p.oid not in
-(select oid from elevated_perm_proc )
+func_with_elevated_privileges AS (
+    SELECT
+        oid,
+        nspname,
+        proname,
+        array_to_string(array_agg(format_type), ',') AS proc_param
+    FROM
+        elevated_perm_procs
+    GROUP BY
+        oid,
+        nspname,
+        proname
+    UNION
+    SELECT
+        p.oid,
+        nspname,
+        proname,
+        ' ' AS proc_param
+    FROM
+        pg_proc p
+    JOIN
+        pg_namespace n ON p.pronamespace = n.oid
+    JOIN
+        pg_authid a ON a.oid = p.proowner
+    WHERE
+        (prosecdef OR NOT proconfig IS NULL)
+        AND p.oid NOT IN (SELECT oid FROM elevated_perm_procs)
 ),
-func_with_elvated_priv1 as
-( select current_database() as dbname,'DATABASE' as level ,nspname||'.'||proname||'('||proc_param||')' as f
-from func_with_elvated_priv 
-            where nspname not in ('dbms_scheduler','dbms_session','pg_catalog','sys','utl_http')
+func_with_elevated_privileges_and_db AS (
+    SELECT
+        current_database() AS dbname,
+        'DATABASE' AS level,
+        nspname || '.' || proname || '(' || proc_param || ')' AS f
+    FROM
+        func_with_elevated_privileges
+    WHERE
+        nspname NOT IN ('dbms_scheduler', 'dbms_session', 'pg_catalog', 'sys', 'utl_http')
 )
-SELECT r.rolname, func.*,'FUNCTION','Elevated Privileges',r.rolcanlogin
-from func_with_elvated_priv1 func
-where has_function_privilege(r.rolname,func.f,'execute')=true;
+SELECT
+    r.rolname,
+    func.*,
+    'FUNCTION' AS object_type,
+    'Elevated Privileges' AS privileges,
+    r.rolcanlogin
+FROM
+    func_with_elevated_privileges_and_db func
+JOIN
+    pg_roles r ON has_function_privilege(r.rolname, func.f, 'execute') = true;
