2834cd772dff1a659c46abe005f275b1d8149850
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 11:54:52 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,3 +1,14 @@
-SELECT r.rolname, current_database(),'DATABASE',nspname||'.'||proname,'FUNCTION','FUNCTION OWNER',r.rolcanlogin
-FROM pg_proc p JOIN pg_namespace n ON p.pronamespace = n.oid 
-and r.oid = p.proowner ;
+SELECT
+    r.rolname AS role_name,
+    current_database() AS database_name,
+    'DATABASE' AS object_type,
+    n.nspname || '.' || p.proname AS object_name,
+    'FUNCTION' AS privilege_type,
+    'FUNCTION OWNER' AS privilege_name,
+    r.rolcanlogin AS can_login
+FROM
+    pg_proc p
+JOIN
+    pg_namespace n ON p.pronamespace = n.oid
+JOIN
+    pg_roles r ON r.oid = p.proowner;
