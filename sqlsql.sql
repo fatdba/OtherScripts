771a9e5710f3d77e0cc190ff54eae4ee4a87b4a9
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 11:28:09 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,18 +1,14 @@
-SELECT
-  r.rolname,
-  current_database() AS current_db,
-  'DATABASE' AS object_type,
-  c.oid::regclass AS object_name,
-  'VIEW' AS object_kind,
-  'VIEW OWNER' AS object_owner,
-  r.rolcanlogin
-FROM
-  pg_class c
-JOIN
-  pg_namespace n ON c.relnamespace = n.oid
-JOIN
-  pg_roles r ON c.relowner = r.oid
-WHERE
-  n.nspname NOT IN ('information_schema', 'pg_catalog', 'sys')
-  AND c.relkind = 'v'
-  AND has_schema_privilege(r.rolname, c.relnamespace, 'USAGE');
+SELECT  r.rolname,current_database(),'DATABASE',c.oid::regclass,'VIEW',
+array(select privs from unnest(ARRAY [
+( CASE WHEN has_table_privilege(r.rolname,c.oid,'SELECT') THEN 'SELECT' ELSE NULL END),
+(CASE WHEN has_table_privilege(r.rolname,c.oid,'INSERT') THEN 'INSERT' ELSE NULL END),
+(CASE WHEN has_table_privilege(r.rolname,c.oid,'UPDATE') THEN 'UPDATE' ELSE NULL END),
+(CASE WHEN has_table_privilege(r.rolname,c.oid,'DELETE') THEN 'DELETE' ELSE NULL END),
+(CASE WHEN has_table_privilege(r.rolname,c.oid,'TRUNCATE') THEN 'TRUNCATE' ELSE NULL END),
+(CASE WHEN has_table_privilege(r.rolname,c.oid,'REFERENCES') THEN 'REFERENCES' ELSE NULL END),
+(CASE WHEN has_table_privilege(r.rolname,c.oid,'TRIGGER') THEN 'TRIGGER' ELSE NULL END)]) foo(privs) where privs is not null) ,
+r.rolcanlogin
+FROM pg_class c JOIN pg_namespace n on c.relnamespace=n.oid where n.nspname not in ('information_schema','pg_catalog','sys') 
+and  c.relkind='v' and has_table_privilege(r.rolname,c.oid,'SELECT, INSERT,UPDATE,DELETE,TRUNCATE,REFERENCES,TRIGGER') 
+AND has_schema_privilege(r.rolname,c.relnamespace,'USAGE')
+and c.relowner <>  r.oid;
