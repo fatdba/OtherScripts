7252057fcba5628190697105e093874d8ce52d13
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 09:50:39 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,5 +1,18 @@
-SELECT  r.rolname,current_database(),'DATABASE',c.oid::regclass,'VIEW','VIEW OWNER' ,
-r.rolcanlogin
-FROM pg_class c JOIN pg_namespace n on c.relnamespace=n.oid where n.nspname not in ('information_schema','pg_catalog','sys') 
-and  c.relkind='v' AND has_schema_privilege(r.rolname,c.relnamespace,'USAGE')
-and c.relowner =  r.oid;
+SELECT
+  r.rolname,
+  current_database() AS current_db,
+  'DATABASE' AS object_type,
+  c.oid::regclass AS object_name,
+  'VIEW' AS object_kind,
+  'VIEW OWNER' AS object_owner,
+  r.rolcanlogin
+FROM
+  pg_class c
+JOIN
+  pg_namespace n ON c.relnamespace = n.oid
+JOIN
+  pg_roles r ON c.relowner = r.oid
+WHERE
+  n.nspname NOT IN ('information_schema', 'pg_catalog', 'sys')
+  AND c.relkind = 'v'
+  AND has_schema_privilege(r.rolname, c.relnamespace, 'USAGE');
