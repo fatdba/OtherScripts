ffc46d11b9402697fb9453240d54e8e11b9f30f9
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 11:30:04 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,14 +1,29 @@
-SELECT  r.rolname,current_database(),'DATABASE',c.oid::regclass,'VIEW',
-array(select privs from unnest(ARRAY [
-( CASE WHEN has_table_privilege(r.rolname,c.oid,'SELECT') THEN 'SELECT' ELSE NULL END),
-(CASE WHEN has_table_privilege(r.rolname,c.oid,'INSERT') THEN 'INSERT' ELSE NULL END),
-(CASE WHEN has_table_privilege(r.rolname,c.oid,'UPDATE') THEN 'UPDATE' ELSE NULL END),
-(CASE WHEN has_table_privilege(r.rolname,c.oid,'DELETE') THEN 'DELETE' ELSE NULL END),
-(CASE WHEN has_table_privilege(r.rolname,c.oid,'TRUNCATE') THEN 'TRUNCATE' ELSE NULL END),
-(CASE WHEN has_table_privilege(r.rolname,c.oid,'REFERENCES') THEN 'REFERENCES' ELSE NULL END),
-(CASE WHEN has_table_privilege(r.rolname,c.oid,'TRIGGER') THEN 'TRIGGER' ELSE NULL END)]) foo(privs) where privs is not null) ,
-r.rolcanlogin
-FROM pg_class c JOIN pg_namespace n on c.relnamespace=n.oid where n.nspname not in ('information_schema','pg_catalog','sys') 
-and  c.relkind='v' and has_table_privilege(r.rolname,c.oid,'SELECT, INSERT,UPDATE,DELETE,TRUNCATE,REFERENCES,TRIGGER') 
-AND has_schema_privilege(r.rolname,c.relnamespace,'USAGE')
-and c.relowner <>  r.oid;
+SELECT
+  r.rolname,
+  current_database() AS current_db,
+  'DATABASE' AS object_type,
+  c.oid::regclass AS object_name,
+  'VIEW' AS object_kind,
+  'VIEW OWNER' AS object_owner,
+  ARRAY(
+    SELECT privs
+    FROM unnest(ARRAY['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'TRUNCATE', 'REFERENCES', 'TRIGGER']) AS privs
+    WHERE
+      (privs = 'SELECT' AND has_table_privilege(r.rolname, c.oid, 'SELECT')) OR
+      (privs = 'INSERT' AND has_table_privilege(r.rolname, c.oid, 'INSERT')) OR
+      (privs = 'UPDATE' AND has_table_privilege(r.rolname, c.oid, 'UPDATE')) OR
+      (privs = 'DELETE' AND has_table_privilege(r.rolname, c.oid, 'DELETE')) OR
+      (privs = 'TRUNCATE' AND has_table_privilege(r.rolname, c.oid, 'TRUNCATE')) OR
+      (privs = 'REFERENCES' AND has_table_privilege(r.rolname, c.oid, 'REFERENCES')) OR
+      (privs = 'TRIGGER' AND has_table_privilege(r.rolname, c.oid, 'TRIGGER'))
+  ) AS object_privileges,
+  r.rolcanlogin
+FROM
+  pg_class c
+  JOIN pg_namespace n ON c.relnamespace = n.oid
+  JOIN pg_roles r ON c.relowner = r.oid
+WHERE
+  n.nspname NOT IN ('information_schema', 'pg_catalog', 'sys')
+  AND c.relkind = 'v'
+  AND has_schema_privilege(r.rolname, c.relnamespace, 'USAGE')
+  AND c.relowner <> r.oid;
