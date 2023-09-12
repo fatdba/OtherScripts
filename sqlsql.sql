fcdd79b9a14e411371ec8c549313548077765d4c
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 11:34:50 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,9 +1,25 @@
-SELECT r.rolname, current_database(),'DATABASE',c.oid::regclass,'SEQUENCE',
-array(select privs from unnest(ARRAY [
-( CASE WHEN has_table_privilege(r.rolname,c.oid,'SELECT') THEN 'SELECT' ELSE NULL END),
-(CASE WHEN has_table_privilege(r.rolname,c.oid,'UPDATE') THEN 'UPDATE' ELSE NULL END)]) foo(privs) where privs is not null) ,r.rolcanlogin
-FROM pg_class c JOIN pg_namespace n on c.relnamespace=n.oid where n.nspname not in ('information_schema','pg_catalog','sys') 
-and  c.relkind='S' and
-has_table_privilege(r.rolname,c.oid,'SELECT,UPDATE')  
-AND has_schema_privilege(r.rolname,c.relnamespace,'USAGE')
-and c.relowner <>  r.oid;
+SELECT
+    r.rolname AS role_name,
+    current_database() AS database_name,
+    'DATABASE' AS object_type,
+    c.oid::regclass AS object_name,
+    'SEQUENCE' AS privilege_type,
+    ARRAY(
+        SELECT unnest(ARRAY[
+            CASE WHEN has_table_privilege(r.rolname, c.oid, 'SELECT') THEN 'SELECT' ELSE NULL END,
+            CASE WHEN has_table_privilege(r.rolname, c.oid, 'UPDATE') THEN 'UPDATE' ELSE NULL END
+        ]) AS privs
+        WHERE privs IS NOT NULL
+    ) AS privileges,
+    r.rolcanlogin AS can_login
+FROM
+    pg_class c
+JOIN
+    pg_namespace n ON c.relnamespace = n.oid
+JOIN
+    pg_roles r ON r.oid = c.relowner
+WHERE
+    n.nspname NOT IN ('information_schema', 'pg_catalog', 'sys')
+    AND c.relkind = 'S'
+    AND has_table_privilege(r.rolname, c.oid, 'SELECT,UPDATE')
+    AND has_schema_privilege(r.rolname, c.relnamespace, 'USAGE');
