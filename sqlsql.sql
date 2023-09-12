dfdfe3b6dc92989276dee53362d40eb326cbc65e
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-12 11:36:25 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,25 +1,34 @@
+WITH Privileges AS (
+    SELECT
+        r.rolname AS role_name,
+        current_database() AS database_name,
+        'DATABASE' AS object_type,
+        c.oid::regclass AS object_name,
+        'SEQUENCE' AS privilege_type,
+        CASE WHEN has_table_privilege(r.rolname, c.oid, 'SELECT') THEN 'SELECT' ELSE NULL END AS select_priv,
+        CASE WHEN has_table_privilege(r.rolname, c.oid, 'UPDATE') THEN 'UPDATE' ELSE NULL END AS update_priv,
+        r.rolcanlogin AS can_login
+    FROM
+        pg_class c
+    JOIN
+        pg_namespace n ON c.relnamespace = n.oid
+    JOIN
+        pg_roles r ON r.oid = c.relowner
+    WHERE
+        n.nspname NOT IN ('information_schema', 'pg_catalog', 'sys')
+        AND c.relkind = 'S'
+        AND has_table_privilege(r.rolname, c.oid, 'SELECT,UPDATE')
+        AND has_schema_privilege(r.rolname, c.relnamespace, 'USAGE')
+)
 SELECT
-    r.rolname AS role_name,
-    current_database() AS database_name,
-    'DATABASE' AS object_type,
-    c.oid::regclass AS object_name,
-    'SEQUENCE' AS privilege_type,
-    ARRAY(
-        SELECT unnest(ARRAY[
-            CASE WHEN has_table_privilege(r.rolname, c.oid, 'SELECT') THEN 'SELECT' ELSE NULL END,
-            CASE WHEN has_table_privilege(r.rolname, c.oid, 'UPDATE') THEN 'UPDATE' ELSE NULL END
-        ]) AS privs
-        WHERE privs IS NOT NULL
-    ) AS privileges,
-    r.rolcanlogin AS can_login
+    role_name,
+    database_name,
+    object_type,
+    object_name,
+    privilege_type,
+    ARRAY_REMOVE(ARRAY[select_priv, update_priv], NULL) AS privileges,
+    can_login
 FROM
-    pg_class c
-JOIN
-    pg_namespace n ON c.relnamespace = n.oid
-JOIN
-    pg_roles r ON r.oid = c.relowner
+    Privileges
 WHERE
-    n.nspname NOT IN ('information_schema', 'pg_catalog', 'sys')
-    AND c.relkind = 'S'
-    AND has_table_privilege(r.rolname, c.oid, 'SELECT,UPDATE')
-    AND has_schema_privilege(r.rolname, c.relnamespace, 'USAGE');
+    ARRAY[select_priv, update_priv] IS NOT NULL;
