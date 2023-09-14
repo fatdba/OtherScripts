1b9f77f096897fe28fe6edfcde413c7048d0bac8
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-09-14 16:02:07 -0400

    Update sqlsql.sql

--- sqlsql.sql
+++ sqlsql.sql
@@ -1,3 +1,10 @@
+import os
+import csv
+import boto3
+from PIL import ImageFont
+from fpdf import FPDF
+from tempfile import NamedTemporaryFile
+
 def generate_csv_and_pdf_reports_for_the_drift_tables(secrets_client, reporting_db_secret_arn, table_names_list, scan_id):
     """
     Function to generate CSV and pdf reports from the tables in the drift reporting database.
@@ -21,13 +28,42 @@ def generate_csv_and_pdf_reports_for_the_drift_tables(secrets_client, reporting_
         print("printresult")
         result_list = [each._asdict() for each in result]
 
-        # Check if result is empty
-        if not result_list:
-            # If the result is empty, create empty CSV and PDF reports
-            header_list = ["No data available"]
-            result_list = [{"No data available": ""}]
-        else:
-            # If the result is not empty, parse it as before
-            header_list = [str(i) for i in result[0]._asdict().keys()]
+        # Always create CSV and PDF reports, even if the result is empty
+        # Create an empty CSV file
+        header_list = [str(i) for i in result[0]._asdict().keys()]
+        Column_sizes = [font.getsize(str(i)) for i in result[0]._asdict().keys()]
+        file_name = str(each_table) + '_scan_' + str(scan_id) + '.csv'
+        file_path = "/tmp/" + file_name
 
-        # ... (Rest of the code remains unchanged)
+        pdf_file_name = str(each_table) + '_scan_' + str(scan_id) + '.pdf'
+        pdf_file_path = "/tmp/" + pdf_file_name
+
+        # Create or update CSV files
+        with open(file_path, "w") as file:
+            writer_object = csv.writer(file)
+            writer_object.writerow(header_list)
+            for each in result_list:
+                writer_object.writerow([str(j) for j in each.values()])
+                # Calculating the column sizes for putting table data into pdf
+                index = 0
+                for i in each.values():
+                    if Column_sizes[index][0] < font.getsize(str(i))[0]:
+                        Column_sizes[index] = font.getsize(str(i))
+                    index += 1
+            file.close()
+
+        # Calculating max cell height and total width of the page
+        total_width = 0
+        max_column_height = 0
+        for each in Column_sizes:
+            total_width += each[0]
+            if max_column_height < each[1]:
+                max_column_height = each[1]
+
+        # Convert CSV file to PDF file
+        convert(file_path, pdf_file_path, align='L', line_height=max_column_height, Column_sizes=Column_sizes,
+                total_width=total_width + 50)
+
+        # Uploading CSV and PDF files to the S3 Bucket
+        s3_client.upload_file(file_path, s3_bucket_name, 'csv_files/{}'.format(file_name))
+        s3_client.upload_file(pdf_file_path, s3_bucket_name, 'pdf_files/{}'.format(pdf_file_name))
