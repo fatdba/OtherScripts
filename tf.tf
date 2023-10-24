7ddebefeaa81cb81e83b500f3c81af661ebcaf7e
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-10-24 11:57:24 -0400

    Update tf.tf

--- tf.tf
+++ tf.tf
@@ -3,20 +3,20 @@ resource "aws_iam_policy" "s3_policy" {
   path        = "/"
 
   policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "s3:*",
-                    "s3-object-lambda:*"
-                ],
-                "Resource": "*"
-            }
-        ]
+      "Effect": "Allow",
+      "Action": [
+        "s3:*",
+        "s3-object-lambda:*"
+      ],
+      "Resource": "*"
     }
-    EOF
+  ]
+}
+EOF
 
   lifecycle {
     prevent_destroy = true
