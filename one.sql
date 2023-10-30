22177a715e08157e95a89e7dcce6ae5dfd3bbf54
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-10-30 07:01:25 -0400

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,10 +1,19 @@
-Error: attaching policy with IAM Policy Attachment AmazonS3FullAccess: – AccessDenied: User: arn:aws:sts::589839611729:assumed-role/cloud-automation/vault-token-terraform-aws-589839611729-1698662801-wnRIb51XozoCoE is not authorized to perform: iam:AttachRolePolicy on resource: role SCLaunchRole with an explicit deny in a service control policy status code: 403, request id: a575183b-0cbc-4cf1-b29e-e89be830edad
-with module.edm_lambda["rds-drift-detection"].aws_iam_policy_attachment.managed_s3_policy_attachment
-on .terraform/modules/edm_lambda/main.tf line 207, in resource "aws_iam_policy_attachment" "managed_s3_policy_attachment":
-resource "aws_iam_policy_attachment" "managed_s3_policy_attachment" {
+The missing permission in your scenario is the permission to perform the iam:AttachRolePolicy action on the SCLaunchRole role. This action allows attaching an IAM policy to an IAM role. The error message explicitly states that the user or role with the ARN arn:aws:sts::589839611729:assumed-role/cloud-automation/vault-token-terraform-aws-589839611729-1698662801-wnRIb51XozoCoE is not authorized to perform this action on the SCLaunchRole role.
 
+To resolve this issue, you need to grant the necessary permission for the user or role to attach policies to the SCLaunchRole role. You can do this by updating the policy associated with the user or role and ensuring it includes the required permissions for the iam:AttachRolePolicy action on the SCLaunchRole resource.
 
-Error: attaching policy with IAM Policy Attachment AmazonS3FullAccess: – AccessDenied: User: arn:aws:sts::589839611729:assumed-role/cloud-automation/vault-token-terraform-aws-589839611729-1698662801-wnRIb51XozoCoE is not authorized to perform: iam:AttachRolePolicy on resource: role SCLaunchRole with an explicit deny in a service control policy status code: 403, request id: c6fc4262-d786-4331-aad2-e7be9bf26348
-with module.edm_lambda["rds-drift-detection"].aws_iam_policy_attachment.s3_policy_attachment
-on .terraform/modules/edm_lambda/main.tf line 215, in resource "aws_iam_policy_attachment" "s3_policy_attachment":
-resource "aws_iam_policy_attachment" "s3_policy_attachment" {
+Here's an example of what the permission might look like in an IAM policy:
+
+json
+Copy code
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "iam:AttachRolePolicy",
+            "Resource": "arn:aws:iam::YOUR_ACCOUNT_ID:role/SCLaunchRole"
+        }
+    ]
+}
+Replace YOUR_ACCOUNT_ID with your AWS account ID. This policy statement allows the specified user or role to attach IAM policies to the SCLaunchRole role. Make sure to attach this policy to the user or role that is trying to perform this action
