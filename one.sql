bd525746f44f31af3a3bc090ee09ce01e8129294
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-11-09 16:19:38 -0500

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,18 +1,33 @@
-rror: Argument or block definition required
-│ 
-│ On .terraform/modules/edm_lambda/main.tf line 35: An argument or block
-│ definition is required here.
-╵
+# Import block for the existing IAM role
+data "external" "import_role" {
+  program = ["sh", "-c", "echo '{\"arn\":\"arn:aws:iam::YOUR_ACCOUNT_ID:role/ROLE_NAME\"}'"]  # Replace YOUR_ACCOUNT_ID and ROLE_NAME
+}
 
-╷
-│ Error: Argument or block definition required
-│ 
-│ On .terraform/modules/edm_lambda/main.tf line 35: An argument or block
-│ definition is required here.
-╵
+# IAM roles and Policies
+# Standard AWS trust policy allowing lambda to assume role
+resource "aws_iam_role" "lambda_role" {
+  count      = (local.create_execution_role == "true") ? 1 : 0
+  name       = local.lambda_execution_role_name
+  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/pubcloud/AppTeamIAMBoundary"
+  assume_role_policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 
-╷
-│ Error: Argument or block definition required
-│ 
-│ On .terraform/modules/edm_lambda/main.tf line 35: An argument or block
-│ definition is required here.
+}
+EOF
+}
+
+# Output the ARN of the imported role
+output "imported_role_arn" {
+  value = jsondecode(data.external.import_role.result)["arn"]
+}
