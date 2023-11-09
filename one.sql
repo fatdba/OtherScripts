a25b55701e3e043537adf4cc91b693050f92bc77
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-11-09 16:05:36 -0500

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,12 +1,23 @@
+# IAM roles and Policies
+# Standard AWS trust policy allowing lambda to assume role
 resource "aws_iam_role" "lambda_role" {
-  # ... other role configuration ...
-}
+  count      = (local.create_execution_role == "true") ? 1 : 0
+  name       = local.lambda_execution_role_name
+  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/pubcloud/AppTeamIAMBoundary"
+  assume_role_policy   = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 
-data "external" "import_role" {
-  program = ["echo", "import_role_id_here"]  # Replace "import_role_id_here" with the actual IAM role's ARN to import
 }
-
-# Output the ARN of the imported role
-output "imported_role_arn" {
-  value = data.external.import_role.result
+EOF
 }
