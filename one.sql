910943e129885fb0989c2f5ecbf6ed857feda8bb
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-11-09 16:11:32 -0500

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,32 +1,7 @@
-# Import block for the existing IAM role
-data "external" "import_role" {
-  program = ["echo", "import_role_id_here"]  # Replace "import_role_id_here" with the actual IAM role's ARN to import
-}
+Error: Unexpected External Program Results
+with module.edm_lambda["rds-drift-detection"].data.external.import_role
+on .terraform/modules/edm_lambda/main.tf line 33, in data "external" "import_role":
+  program = ["echo", "arn:aws:iam::219586591115:role/drift_detection"]  # Replace "import_role_id_here" with the actual IAM role's ARN to import
+The data source received unexpected results after executing the program.
 
-# IAM roles and Policies
-# Standard AWS trust policy allowing lambda to assume role
-resource "aws_iam_role" "lambda_role" {
-  count      = (local.create_execution_role == "true") ? 1 : 0
-  name       = local.lambda_execution_role_name
-  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/pubcloud/AppTeamIAMBoundary"
-  assume_role_policy   = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-# Output the ARN of the imported role
-output "imported_role_arn" {
-  value = data.external.import_role.result
-}
+Program output must be a JSON encoded map of string keys and string values.
