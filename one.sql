2d0beba5a2ad37b04035243327add83665db64ee
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-11-09 16:13:34 -0500

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,7 +1,7 @@
-Error: Unexpected External Program Results
-with module.edm_lambda["rds-drift-detection"].data.external.import_role
-on .terraform/modules/edm_lambda/main.tf line 33, in data "external" "import_role":
-  program = ["echo", "arn:aws:iam::219586591115:role/drift_detection"]  # Replace "import_role_id_here" with the actual IAM role's ARN to import
-The data source received unexpected results after executing the program.
+data "external" "import_role" {
+  program = ["sh", "-c", "echo '{\"arn\":\"arn:aws:iam::YOUR_ACCOUNT_ID:role/ROLE_NAME\"}'"]  # Replace YOUR_ACCOUNT_ID and ROLE_NAME
+}
 
-Program output must be a JSON encoded map of string keys and string values.
+output "imported_role_arn" {
+  value = jsondecode(data.external.import_role.result).arn
+}
