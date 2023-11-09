237d8fa59b86ea0159c242f1027db03f0ea7a7b0
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2023-11-09 16:23:44 -0500

    Update one.sql

--- one.sql
+++ one.sql
@@ -1,33 +1,5 @@
-# Import block for the existing IAM role
-data "external" "import_role" {
-  program = ["sh", "-c", "echo '{\"arn\":\"arn:aws:iam::YOUR_ACCOUNT_ID:role/ROLE_NAME\"}'"]  # Replace YOUR_ACCOUNT_ID and ROLE_NAME
-}
-
-# IAM roles and Policies
-# Standard AWS trust policy allowing lambda to assume role
-resource "aws_iam_role" "lambda_role" {
-  count      = (local.create_execution_role == "true") ? 1 : 0
-  name       = local.lambda_execution_role_name
-  permissions_boundary = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/pubcloud/AppTeamIAMBoundary"
-  assume_role_policy   = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-
-}
-EOF
-}
-
-# Output the ARN of the imported role
-output "imported_role_arn" {
+Error: Invalid function argument
+on .terraform/modules/edm_lambda/main.tf line 63, in output "imported_role_arn":
   value = jsondecode(data.external.import_role.result)["arn"]
-}
+data.external.import_role.result is map of string with 1 element
+Invalid value for "str" parameter: string required.
