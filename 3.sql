9e7aec293ccde42691f0fdd9619963bdf2e760ce
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2024-01-30 17:14:51 -0500

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,5 +1,46 @@
-Hello Mike,
 
-I reached out to AWS Support to inquire about the feasibility of physical streaming replication between an on-premises source and an RDS or Aurora PostgreSQL target. According to their response, this type of replication is not supported due to the necessity of access to the underlying host and various other limitations. Regrettably, there is no formal document available, but all the relevant information is recorded in the ticket history. They suggested presenting this information to anyone seeking details about the limitation.
 
-The support case, including the entire conversation and the final decision from the RDS & Aurora support team regarding replication, is outlined below. Kindly review it, and if there are any lingering uncertainties, feel free to let me know. I've instructed the engineer to keep the ticket open for a few more days to address any additional questions.
+Using DMS which uses replication instance & Replication tasks,
+
+
+apprximate cost for this kind of setup
+transit gateways
+-----------------------------------------
+
+paying for running RDS - storage cost, licnes cost and size of the instance, instance type, IO types etc. 
+Data replication (data transfer cost) : No cost.
+
+
+
+https://aws.amazon.com/rds/postgresql/pricing/
+
+https://calculator.aws/#/createCalculator/RDSPostgreSQL
+
+m4.10x large
+single az
+pricing model - ondemand or reserved (ondem) 
+no need of an RDS proxy here.
+storage : gp2 
+
+
+
+
+Selected Instance:
+db.m4.4xlarge
+vCPU: 16
+Memory: 64 GiB
+
+1 instance(s) x 1.461 USD hourly x (100 / 100 Utilized/Month) x 730 hours in a month = 1066.5300 USD
+Amazon RDS PostgreSQL instances cost (monthly): 1,066.53 USD
+
+
+100 GB per month x 0.115 USD x 1 instances = 11.50 USD (Storage Cost)
+Storage pricing (monthly): 11.50 USD
+
+nothing new to pay in case of any existing configuration. 
+
+
+DMS (addtiinal cost): 
+pub-sub kinda arch
+
+
