d1599e7979a5b255b0637744b54d5742bc052055
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2024-01-30 17:22:12 -0500

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,46 +1,13 @@
+I have completed my analysis to obtain costing information for the logical replication approach, and the following highlights encompass each of the involved components:
 
+In the scenario where the primary instance resides in the customer's on-premises data center, the costs will be associated with running RDS, including storage cost, licensing cost, instance size, instance type, IO types, etc.
 
-Using DMS which uses replication instance & Replication tasks,
+No data replication costs will be incurred.
 
+If we opt for the "db.m4.4xlarge" type for the RDS instance (16 vCPUs) with 64 GB memory, a single AZ, no RDS Proxy, and an on-demand instance, the monthly cost for Amazon RDS PostgreSQL instances will be $1,066.53. This is calculated as follows: 1 instance(s) x $1.461 hourly x (100% utilization/month) x 730 hours in a month = $1,066.53.
 
-apprximate cost for this kind of setup
-transit gateways
------------------------------------------
+Assuming 100 GB storage with the general-purpose gp2 type, the monthly storage cost will be $11.50: 100 GB per month x $0.115 x 1 instance = $11.50 (Storage Cost).
 
-paying for running RDS - storage cost, licnes cost and size of the instance, instance type, IO types etc. 
-Data replication (data transfer cost) : No cost.
+It is assumed that the network configuration between on-premises and AWS infrastructure already exists, and no new networking components such as Transit Gateways, networking rules, or objects need to be created.
 
-
-
-https://aws.amazon.com/rds/postgresql/pricing/
-
-https://calculator.aws/#/createCalculator/RDSPostgreSQL
-
-m4.10x large
-single az
-pricing model - ondemand or reserved (ondem) 
-no need of an RDS proxy here.
-storage : gp2 
-
-
-
-
-Selected Instance:
-db.m4.4xlarge
-vCPU: 16
-Memory: 64 GiB
-
-1 instance(s) x 1.461 USD hourly x (100 / 100 Utilized/Month) x 730 hours in a month = 1066.5300 USD
-Amazon RDS PostgreSQL instances cost (monthly): 1,066.53 USD
-
-
-100 GB per month x 0.115 USD x 1 instances = 11.50 USD (Storage Cost)
-Storage pricing (monthly): 11.50 USD
-
-nothing new to pay in case of any existing configuration. 
-
-
-DMS (addtiinal cost): 
-pub-sub kinda arch
-
-
+DMS-based logical replication setup is not considered, and native PostgreSQL replication methods will be used to avoid additional costs for replication instances, storage, and computation.
