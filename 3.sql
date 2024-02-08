98bac8e3881ece6b51a1a978502e7e3840691ea8
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2024-02-08 15:54:09 -0500

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,6 +1,1 @@
-original_log="/path/to/original_alert_log_file"
-new_log="/path/to/new_alert_log_file"
-
-# Extract the last 5 days' entries and redirect them to a new file
-grep "$(date -d '5 days ago' '+%Y-%m-%d')" "$original_log" | \
-  awk -v d="$(date -d '5 days ago' '+%Y-%m-%d')" '$1 >= d' > "$new_log"
+grep "$(date -d '5 days ago' '+%Y-%m-%d')" /path/to/alert_log_file | awk -v d="$(date -d '5 days ago' '+%Y-%m-%d')" '$1 >= d'
