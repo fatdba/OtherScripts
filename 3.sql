e06631fc66011ef438236c81611896d8a9f60e2b
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2024-02-08 15:56:24 -0500

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,1 +1,6 @@
-grep "$(date -d '5 days ago' '+%Y-%m-%d')" /path/to/alert_log_file | awk -v d="$(date -d '5 days ago' '+%Y-%m-%d')" '$1 >= d'
+# Set the path to the original alert log file and the new file
+original_log="/path/to/original_alert_log_file"
+new_log="/path/to/new_alert_log_file"
+
+# Extract all entries from the last 5 days and redirect them to a new file
+sed -n "/$(date -d '5 days ago' '+%Y-%m-%d')/,/$(date '+%Y-%m-%d')/p" "$original_log" > "$new_log"
