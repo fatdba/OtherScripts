7ab5aa781a1488b244f82b72afe35fd1c9ca301c
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2024-03-13 14:49:52 -0400

    Update 3.sql

--- 3.sql
+++ 3.sql
@@ -1,1 +1,1 @@
-https://www.youtube.com/watch?v=ZJcXI-k_en0
+botocore.exceptions.ClientError: An error occurred (AccessDeniedException) when calling the GetSecretValue operation: User: arn:aws:sts::975050016786:assumed-role/ec2_ssm_edm-ops/i-0e45527e5a2a41beb is not authorized to perform: secretsmanager:GetSecretValue on resource: arn:aws:secretsmanager:us-east-2:589839611729:secret:secret/michaeltestc-db/rds-password-gok7XS becauseno resource-based policy allows the secretsmanager:GetSecretValue action
