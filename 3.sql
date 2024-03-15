a3e327016d3b37861f95c72fe80168766695ae43
Prashant Dixit <34828819+fatdba@users.noreply.github.com>
2024-03-15 08:50:02 -0400

    Delete 3.sql

--- 3.sql
+++
@@ -1,4 +0,0 @@
-ws s3 cp s3:edm-saas-dev-arrxi/2024-03-13/edm-drift_detection.py /tmp/edm-drift_detection.py
-
-usage: aws s3 cp <LocalPath> <S3Uri> or <S3Uri> <LocalPath> or <S3Uri> <S3Uri>
-Error: Invalid argument type
